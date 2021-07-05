f00c7e43a796b6ae72cabf3055321e64f13689b3
Karpovich_alex_mpc <karpovehalex@gmail.com>
2021-07-05 17:13:47 +0300

    Add some api methods

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import logging
 from json import dumps
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-from database import Handler, Message, Bot, init_db
+from database import Handler, Bot, init_db, Chat, User
 from utils import Connector
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 init_db()
 app = FastAPI()
 
@@ -24,21 +27,20 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+ERROR = {"status": "error"}
 
 class AcptMessage(BaseModel):
     text: str
     chat_id: int
-    bot_id: int
+    user_id: int
     # TODO: Change type
     time: str
 
     def to_json(self):
-        return dumps({"text": self.text, "chat_id": self.chat_id, "bot_id": self.bot_id,
-                      "time": self.time})
+        return dumps({"text": self.text, "chat_id": self.chat_id, "time": self.time, "user_id": self.user_id})
 
 
-connector = Connector('send')
+connector = Connector("send")
 
 
 @Handler.request_decorator
@@ -49,22 +51,39 @@ def get_bots():
     return {"bots": bots_id}
 
 
+@app.get("/{bot_id}/users")
+def get_support_users(bot_id: int):
+    support_users = User.get_support_users(bot_id)
+    return {'support_users': [{'username': u.username, 'id': u.id} for u in support_users]}
+
+
 @app.get("/{bot_id}/chats")
 def get_chats(bot_id: int):
-    messages = Bot.get_messages(bot_id)
-    return {"bot_id": bot_id, "messages": [message.to_dict() for message in messages]}
+    chats = Bot.get_chats(bot_id)
+    return {"bot_id": bot_id, "chats": [chat.to_dict_last() for chat in chats]}
 
 
-@app.get("/{bot_id}/messages/{user_id}")
-async def get_messages(bot_id: int, user_id: int):
-    messages = Message.get_messages(bot_id, user_id)
-    return {"user_id": user_id, "messages": [message.to_dict() for message in messages]}
+@app.get("/messages/{chat_id}")
+async def get_messages(chat_id: int):
+    chat = Chat.get_chat(chat_id)
+    if not chat:
+        return {"status": "error"}
+    messages = chat.get_all_messages()
+    return {"chat_id": chat_id, "messages": [message.to_dict() for message in messages]}
 
 
-@app.post("/{bot_id}/messages/{user_id}")
-async def send_message(bot_id: int, user_id: int, msg: AcptMessage):
+@app.post("/messages/{chat_id}")
+async def send_message(chat_id: int, msg: AcptMessage):
     print("CATCH", msg.text)
+    if msg.chat_id != chat_id:
+        return ERROR
     if connector.publish(msg.to_json()):
         return {"status": "send"}
     else:
-        return {"status": "error"}
+        return ERROR
+
+# def parse_msg(msg: AcptMessage):
+#
+#     chat=Chat.get_chat(msg.chat_id)
+#     bot_id = chat.bot.messenger_id
+#     messe
